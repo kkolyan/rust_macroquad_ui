6ff6a7a764753a3477107e99038faf51da5d53dc
kkolyan <kkolyankkolyan@gmail.com>
2022-09-13 21:32:45 +0400

    refactor example1

--- examples/example1.rs
+++ examples/example1.rs
@@ -22,12 +22,55 @@ enum Event {}
 
 #[macroquad::main("UI Example 001")]
 async fn main() {
+    loop {
+        clear_background(BLACK);
+        let root = root();
+        let _ = rust_macroquad_ui::core::collect_layer_events(&root);
+        rust_macroquad_ui::core::draw_layer(&root);
+        next_frame().await;
+    }
+}
+
+fn root() -> Node<Event> {
     let text_1 = TextStyle {
         font_size: 32.0,
         color: WHITE,
     };
 
-    let left_panel = Node::new("Left panel")
+    Node::<Event>::new("root")
+        .group(Layout::Horizontal, vec![
+            lef_panel(text_1),
+            Node::new("stretch")
+                .height(0.0)
+                .width_stretch(),
+            Node::new("Right block")
+                .group(Layout::Vertical, vec![
+                    Node::new("stretch").height_stretch().width(0.0),
+                    right_bottom_panel(),
+                ]),
+        ])
+}
+
+fn right_bottom_panel() -> Node<Event> {
+    Node::new("margin")
+        .background_from_color(WHITE)
+        .margin(
+            MarginOffset::from(8.0),
+            Node::new("right bottom panel")
+                .group(
+                    Layout::Horizontal,
+                    vec![RED, ORANGE, YELLOW, GREEN, BLUE, DARKBLUE, PURPLE].iter()
+                        .map(|color| Node::new("color icon")
+                            .margin(
+                                MarginOffset::from((8.0, 0.0)),
+                                Node::new("icon").background_from_color(*color).width(32.0).height(32.0),
+                            )).collect(),
+                ),
+        )
+}
+
+fn lef_panel(text_1: TextStyle) -> Node<Event> {
+    Node::new("Left panel")
         .background_from_color(GREEN)
         .group(Layout::Vertical, vec![
             Node::new("minimap frame")
@@ -59,41 +102,5 @@ async fn main() {
                                 .collect()),
                     ]),
                 ),
-        ]);
-
-    let right_bottom_panel = Node::new("margin")
-        .background_from_color(WHITE)
-        .margin(
-            MarginOffset::from(8.0),
-            Node::new("right bottom panel")
-                .group(
-                    Layout::Horizontal,
-                    vec![RED, ORANGE, YELLOW, GREEN, BLUE, DARKBLUE, PURPLE].iter()
-                        .map(|color| Node::new("color icon")
-                            .margin(
-                                MarginOffset::from((8.0, 0.0)),
-                                Node::new("icon").background_from_color(*color).width(32.0).height(32.0),
-                            )).collect(),
-                ),
-        );
-
-    let root = Node::<Event>::new("root")
-        .group(Layout::Horizontal, vec![
-            left_panel,
-            Node::new("stretch")
-                .height(0.0)
-                .width_stretch(),
-            Node::new("Right block")
-                .group(Layout::Vertical, vec![
-                    Node::new("stretch").height_stretch().width(0.0),
-                    right_bottom_panel,
-                ]),
-        ]);
-
-    loop {
-        clear_background(BLACK);
-        let _ = rust_macroquad_ui::core::collect_layer_events(&root);
-        rust_macroquad_ui::core::draw_layer(&root);
-        next_frame().await;
-    }
+        ])
 }
